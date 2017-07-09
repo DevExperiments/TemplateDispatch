90f9188d72e066e3e80983b47789e6af5137b8ab
DevExperiments <andydominy@gmail.com>
2017-07-09 12:08:40 +0100

    Formatting, vector include & namespace
    Aligned tab usage. Corrected vector header include and added std namespace.

--- src/dispatch.h
+++ src/dispatch.h
@@ -2,27 +2,27 @@
 #ifndef DispatchH
 #define DispatchH
 //---------------------------------------------------------------------------
-#include <vector.h>
+#include <vector>
 
 template <typename TData>
 class TDispatcher
 {
 public:
-   __fastcall ~TDispatcher (void) {
-      oDispatchHandlers.clear ();
-   }
+	__fastcall ~TDispatcher (void) {
+		oDispatchHandlers.clear ();
+	}
 
-   struct TDispatchHandler {
-      typedef void __fastcall (__closure * FnOnDispatch) (TData& oData);
-      typedef FnOnDispatch TDispatchFnType;
-      FnOnDispatch OnDispatch;
+	struct TDispatchHandler {
+	  	typedef void __fastcall (__closure * FnOnDispatch) (TData& oData);
+	  	typedef FnOnDispatch TDispatchFnType;
+	  	FnOnDispatch OnDispatch;
 
-      __fastcall TDispatchHandler (void) : OnDispatch(NULL) {}
-      __fastcall TDispatchHandler (FnOnDispatch pFnOnDispatch) : OnDispatch(pFnOnDispatch) {}
-      __fastcall ~TDispatchHandler (void) { OnDispatch = NULL; }
-   };
+	  	__fastcall TDispatchHandler (void) : OnDispatch(NULL) {}
+	  	__fastcall TDispatchHandler (FnOnDispatch pFnOnDispatch) : OnDispatch(pFnOnDispatch) {}
+	  	__fastcall ~TDispatchHandler (void) { OnDispatch = NULL; }
+	};
 
-   typedef TData TDispatchData;
+   	typedef TData TDispatchData;
    
 	void __fastcall Subscribe (const TDispatchHandler& oCallbackData) {
 		oDispatchHandlers.push_back (oCallbackData);
@@ -43,21 +43,21 @@ public:
 	}
 
 private:
-	typedef vector<TDispatchHandler> TDispatchHandlers;
+	typedef std::vector<TDispatchHandler> TDispatchHandlers;
 	typedef TDispatchHandlers::iterator TDispatchHandlersIterator;
-  typedef TDispatchHandlers::const_iterator TDispatchHandlersConstIterator;
+  	typedef TDispatchHandlers::const_iterator TDispatchHandlersConstIterator;
 
 	TDispatchHandlers oDispatchHandlers;
 
-   inline void __fastcall FireCallbackX (TData& oData, TDispatchHandler& oDispatchFn) {
-      TDispatchHandler::TDispatchFnType Fn = oDispatchFn.OnDispatch;
-      if (Fn) Fn (oData);
-   }
+   	inline void __fastcall FireCallbackX (TData& oData, TDispatchHandler& oDispatchFn) {
+      	TDispatchHandler::TDispatchFnType Fn = oDispatchFn.OnDispatch;
+      	if (Fn) Fn (oData);
+   	}
 
-   inline void __fastcall FireCallbackX (const TData& oData, TDispatchHandler& oDispatchFn) const {
-      TDispatchHandler::TDispatchFnType Fn = oDispatchFn.OnDispatch;
-      if (Fn) Fn (oData);
-   }
+   	inline void __fastcall FireCallbackX (const TData& oData, TDispatchHandler& oDispatchFn) const {
+      	TDispatchHandler::TDispatchFnType Fn = oDispatchFn.OnDispatch;
+      	if (Fn) Fn (oData);
+   	}
 };
 //---------------------------------------------------------------------------
 #endif
